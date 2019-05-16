a66e782d4bd315b29476b89f7eacaedb3d80f2de
Martin Czygan <martin.czygan@gmail.com>
2019-05-17 01:36:58 +0200

    be case insensitive, fixes #1
    * Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
    * Examples (base sensitivity): a ≠ b, a = á, a = A.

--- index.js
+++ index.js
@@ -69,7 +69,9 @@
         let result = [];
         for (let i = 0; i < a.length; i++) {
             for (let j = 0; j < b.length; j++) {
-                if (a[i] === b[j]) {
+                if (a[i].localeCompare(b[j], undefined, {
+                        sensitivity: "base"
+                    }) === 0) {
                     result.push(a[i]);
                     break;
                 }
